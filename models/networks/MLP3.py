6e09129879b5739c9bead10268d5ec6418efc994
Xun Wang <stephenwang016@gmail.com>
2022-04-18 23:08:38 +0200

    fixed typo

--- models/networks/MLP3.py
+++ models/networks/MLP3.py
@@ -9,14 +9,17 @@ from models.Pruneable import Pruneable
 class MLP3(Pruneable):
     def __init__(self,device="cuda",hidden_dim=64,output_dim=10,input_dim=(1,),**kwargs):
         super(MLP3,self).__init__(device=device,output_dim=output_dim,input_dim=input_dim,**kwargs)
-        hidden_dim = hidden_dim[0]
-        input_dim = int(np.prod(input_dim))
+        self.hidden_dim = hidden_dim[0]
+        self.input_dim = int(np.prod(input_dim))
+        self.output_dim = output_dim
         gain=1
         self.layers = nn.Sequential(
             self.Linear(input_dim=input_dim,output_dim=hidden_dim,bias=True,gain=gain),
+            nn.ReLU(),
             self.Linear(input_dim=hidden_dim,output_dim=hidden_dim,bias=True,gain=gain),
-            self.Linear(input=hidden_dim,output_dim=output_dim,bias=True,gain=gain)
-        )
+            nn.ReLU(),
+            self.Linear(input_dim=hidden_dim,output_dim=output_dim,bias=True,gain=gain)
+        ).to(device)
         
     
     def forward(self,x:torch.Tensor):
