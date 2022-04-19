6dcb51d5b27274dca4acd1193d9d43b949f53ef4
Xun Wang <stephenwang016@gmail.com>
2022-04-19 19:09:21 +0200

    updated mlp3

--- models/networks/MLP3.py
+++ models/networks/MLP3.py
@@ -9,8 +9,8 @@ from models.Pruneable import Pruneable
 class MLP3(Pruneable):
     def __init__(self,device="cuda",hidden_dim=64,output_dim=10,input_dim=(1,),**kwargs):
         super(MLP3,self).__init__(device=device,output_dim=output_dim,input_dim=input_dim,**kwargs)
-        self.hidden_dim = hidden_dim[0]
-        self.input_dim = int(np.prod(input_dim))
+        hidden_dim = hidden_dim
+        input_dim = int(np.prod(input_dim))
         self.output_dim = output_dim
         gain=1
         self.layers = nn.Sequential(
